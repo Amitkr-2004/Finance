61f7157168efc3edf8be89282814caeb458ed64a
Amitkr-2004 <amitkumar990374@gmail.com>
2025-09-14 11:20:47 +0530

    Added models of Transaction and User

--- Expense Tracker/Backend/server.js
+++ Expense Tracker/Backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    console.log('✅ Connected to MongoDB');
+    console.log('Connected to MongoDB');
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
