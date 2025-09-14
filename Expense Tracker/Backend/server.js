866bab83568c44b4a39511c1d4adfb61e3e751b4
Amitkr-2004 <amitkumar990374@gmail.com>
2025-09-14 11:14:20 +0530

    backend folder is created and server.js is initialized

---
+++ Expense Tracker/Backend/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+});
